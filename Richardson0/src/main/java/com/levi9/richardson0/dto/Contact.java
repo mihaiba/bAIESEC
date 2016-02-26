88579211075eda23e1e8cc8d20127126f7c404d1
Mihai Balaniscu <m.balaniscu@levi9.com>
2016-02-26 13:00:56 +0200

    rename package

---
+++ Richardson0/src/main/java/com/levi9/richardson0/dto/Contact.java
@@ -0,0 +1,40 @@
+package com.levi9.richardson0.dto;
+
+public class Contact {
+	private int id;
+	private String fName;
+	private String lName;
+	private String phoneNumber;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getfName() {
+		return fName;
+	}
+
+	public void setfName(String fName) {
+		this.fName = fName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
---
+++ Richardson0/src/main/java/com/levi9/richardson0/dto/User.java
@@ -0,0 +1,33 @@
+package com.levi9.richardson0.dto;
+
+import java.util.List;
+
+public class User {
+	private int id;
+	private String name;
+	private List<Contact> contacts;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Contact> getContacts() {
+		return contacts;
+	}
+
+	public void setContacts(List<Contact> contacts) {
+		this.contacts = contacts;
+	}
+}
